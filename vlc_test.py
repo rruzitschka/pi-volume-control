45b1aa8f62e171361aa4d7ab3929e21c5ad84712
Robert Ruzitschka <rruzitschka@gmail.com>
2021-11-21 16:42:48 +0100

    added volume settings

--- vlc_test.py
+++ vlc_test.py
@@ -5,5 +5,9 @@ songPath='/home/pi/raspi-dev/SoundHelix-Song-1.mp3'
 
 p=vlc.MediaPlayer(songPath)
 p.play()
+for i in range(100):
+    result = p.audio_set_volume(i)
+    print("Volume:",i, "result:", result)
+    time.sleep(2)
 time.sleep(10)
 p.stop()
