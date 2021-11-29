95e3de3f501595ab19b87fd317bfa2254268d2e3
Robert Ruzitschka <rruzitschka@gmail.com>
2021-11-29 11:31:08 +0100

    added volume change to test

--- pygame_test.py
+++ pygame_test.py
@@ -6,6 +6,12 @@ pygame.mixer.music.load("/home/pi/raspi-dev/rain.mp3")
 pygame.mixer.music.play(-1)
 
 while True:
-    time.sleep (60)
-
+   
+    for volume in range(100):
+        pygame.mixer.set_volume(volume/100)
+        time.sleep(5)
+    for volume in range(100):
+        pygame.mixer.set_volume(1 - volume/100)
+        time.sleep(5)
+        
 pygame.mixer.stop()   
